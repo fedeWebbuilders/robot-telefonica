a5af28d4aaea40e279bac3d3ef3b8d3ba6f43ec3
unknown <you@example.com>
2026-03-09 11:40:57 +0100

    initial

---
+++ run.ps1
@@ -0,0 +1,8 @@
+# Cambia a la carpeta del proyecto
+cd "C:\Users\Usuario\Desktop\Robot v2\robot-telefonica"
+
+# Lanza el comando de inicio
+npm start
+
+# Mantiene la ventana abierta si hay un error para que puedas leerlo
+Read-Host -Prompt "Presiona Enter para salir"
